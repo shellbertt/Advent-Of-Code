01103118ee50439b0ef54e623230ae8ca6961a36
shellbertt <kaadenruman@hotmail.com>
2022-11-30 19:21:29 -0700

    template

--- aoc2022/1.py
+++ aoc2022/1.py
@@ -1,1 +1,8 @@
-print(input())
+import sys, collections, itertools, math
+input = lambda: sys.stdin.readline().rstrip()
+ints = lambda: list(map(int, sys.stdin.readlines()))
+strs = lambda: list(lambda l: l.rstrip(), sys.stdin.readlines)
+Int = lambda: int(input())
+yn = lambda b: print('YES' if b else 'NO')
+
+
