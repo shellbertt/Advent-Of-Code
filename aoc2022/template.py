ffb96f5b58a1f079a124710e09c1ef6510062831
shellbertt <kaadenruman@hotmail.com>
2022-12-03 23:48:47 -0700

    more template crafting

--- aoc2022/template.py
+++ aoc2022/template.py
@@ -1,17 +1,18 @@
 import sys, collections, functools, itertools, math, re
-ints = lambda s=None: lmap(maybeint, input().split(s))
+Ints = lambda s=input(): lmap(maybeint, re.findall(r"\d+", s))
 Int = lambda: int(input())
 eg = ''''''
-stdin = sys.stdin.read().rstrip()
+stdin = lambda: sys.stdin.read().rstrip()
 #################
-if eg: stdin = eg
+if eg: stdin = lambda: eg
 #################
 lmap = lambda f, *x: list(map(f, *x))
 maybeint = lambda x: int(x) if x.isnumeric() else x
 ints = lambda: lmap(maybeint, strs())
 grid = lambda: lmap(lambda x: lmap(maybeint, x), strs())
-strs = lambda s=stdin: s.splitlines()
+strs = lambda: stdin().splitlines()
 splits = lambda s=None: lmap(lambda x: lmap(maybeint, x.split(s)), strs())
-chunks = lambda: lmap(lambda c: lmap(maybeint, c.splitlines()), stdin.split('\n\n'))
+chunks = lambda: lmap(lambda c: lmap(maybeint, c.splitlines()), stdin().split('\n\n'))
+slide = lambda l, n, s=1: [l[i:i + n] for i in range(0, len(l) - n, s)]
 
 
