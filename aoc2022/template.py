13c63df5a7a2c3bc6b742ea1bf94e956aaa3dfa0
shellbertt <kaadenruman@hotmail.com>
2022-12-03 23:37:13 -0700

    more template crafting cd /home/kaaden/cp/Advent-Of-Code/aoc2022 ; /usr/bin/env /bin/python3 /home/kaaden/.vscode-server/extensions/ms-python.python-2022.18.2/pythonFiles/lib/python/debugpy/adapter/../../debugpy/launcher 43379 -- /home/kaaden/cp/Advent-Of-Code/aoc2022/5.py < 5.in

--- aoc2022/template.py
+++ aoc2022/template.py
@@ -1,17 +1,17 @@
 import sys, collections, functools, itertools, math, re
-input = lambda: sys.stdin.readline().rstrip()
+ints = lambda s=None: lmap(maybeint, input().split(s))
 Int = lambda: int(input())
-yn = lambda b: print('YES' if b else 'NO')
 eg = ''''''
 stdin = sys.stdin.read().rstrip()
 #################
 if eg: stdin = eg
 #################
-maybeint = lambda x: int(x) if x.isnumeric() else x
 lmap = lambda f, *x: list(map(f, *x))
-ints = lambda: lmap(maybeint, stdin.splitlines())
-strs = lambda: stdin.splitlines()
-splits = lambda s: lmap(lambda x: lmap(maybeint, x.split(s)), stdin.splitlines())
+maybeint = lambda x: int(x) if x.isnumeric() else x
+ints = lambda: lmap(maybeint, strs())
+grid = lambda: lmap(lambda x: lmap(maybeint, x), strs())
+strs = lambda s=stdin: s.splitlines()
+splits = lambda s=None: lmap(lambda x: lmap(maybeint, x.split(s)), strs())
 chunks = lambda: lmap(lambda c: lmap(maybeint, c.splitlines()), stdin.split('\n\n'))
 
 
