713b60206bfb645e27cd9f101e90ff350c53e54e
shellbertt <kaadenruman@hotmail.com>
2022-12-09 22:34:21 -0700

    Day 10, messiest day yet, very slow, I blame no sleep last night

---
+++ aoc2022/10.py
@@ -0,0 +1,189 @@
+import sys, collections, functools, itertools, math, re
+words = lambda s: re.findall(r"[a-zA-z]+", s)
+Ints = lambda s: lmap(maybeint, re.findall(r"\d+", s))
+Int = lambda: int(input())
+eg = '''addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+noop
+addx 3
+addx -5
+'''
+stdin = lambda: sys.stdin.read().rstrip()
+#################
+# if eg: stdin = lambda: eg
+#################
+lmap = lambda f, *x: list(map(f, *x))
+maybeint = lambda x: int(x) if x.isnumeric() else x
+ints = lambda: lmap(maybeint, strs())
+grid = lambda: lmap(lambda x: lmap(maybeint, x), strs())
+strs = lambda: stdin().splitlines()
+splits = lambda s=None: lmap(lambda x: lmap(maybeint, x.split(s)), strs())
+chunks = lambda: lmap(lambda c: lmap(maybeint, c.splitlines()), stdin().split('\n\n'))
+slide = lambda l, n, s=1: [l[i:i + n] for i in range(0, len(l) - n, s)]
+
+ans = 0
+x = 1
+i = 1
+seen = set()
+for s in splits():
+  if s[0] == 'noop':
+    i += 1
+    continue
+  if (i + 20) % 40 == 0 and i not in seen:
+    seen.add(i)
+    ans += i * x
+  i += 1
+  if (i + 20) % 40 == 0 and i not in seen:
+    seen.add(x)
+    ans += i * x
+  x += int(s[1])
+  i += 1
+  if (i + 20) % 40 == 0 and i not in seen:
+    seen.add(i)
+    ans += i * x
+
+print(ans)
