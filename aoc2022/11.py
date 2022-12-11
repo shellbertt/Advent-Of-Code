e57b05a3b03a62cb690e34ffa7292925203411ba
shellbertt <kaadenruman@hotmail.com>
2022-12-10 23:09:02 -0700

    Day 11, was being dumb on part 2 and hand monstrously slow code

---
+++ aoc2022/11.py
@@ -0,0 +1,67 @@
+import sys, collections, functools, itertools, math, re
+words = lambda s: re.findall(r"[a-zA-z]+", s)
+Ints = lambda s: lmap(maybeint, re.findall(r"\d+", s))
+Int = lambda: int(input())
+eg = '''Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
+'''
+stdin = lambda: sys.stdin.read().rstrip()
+#################
+# if eg: stdin = lambda: eg
+#################
+lmap = lambda f, *x: list(map(f, *x))
+maybeint = lambda x: int(x) if re.match(r"[+-]?\d+", x) else x
+ints = lambda: lmap(maybeint, strs())
+grid = lambda: lmap(lambda x: lmap(maybeint, x), strs())
+strs = lambda: stdin().splitlines()
+splits = lambda s=None: lmap(lambda x: lmap(maybeint, x.split(s)), strs())
+chunks = lambda: lmap(lambda c: lmap(maybeint, c.splitlines()), stdin().split('\n\n'))
+slide = lambda l, n, s=1: [l[i:i + n] for i in range(0, len(l) - n, s)]
+
+m = []
+M = []
+C = chunks()
+for c in C:
+  m.append(Ints(c[1]))
+  M.append([c[2].split(': ')[1], Ints(c[3])[0], Ints(c[4])[0], Ints(c[5])[0]])
+
+mc = len(m)
+mic = [0] * mc
+
+for _ in range(20):
+  for i in range(len(m)):
+    while len(m[i]) > 0:
+      new = None
+      old = m[i].pop(0)
+      exec(M[i][0])
+      new //= 3
+      m[M[i][2] if new % M[i][1] == 0 else M[i][3]].append(new)
+      mic[i] += 1
+      
+ans = sorted(mic)
+print(ans[-1] * ans[-2])
