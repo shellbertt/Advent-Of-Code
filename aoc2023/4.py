a186482d4d0e359fdb44c7852169503b83701a64
Kaaden RumanCam <kaadenruman@hotmail.com>
2023-12-04 00:19:35 -0500

    Day 4, just missed my goal of top 100... one day

---
+++ aoc2023/4.py
@@ -0,0 +1,24 @@
+import sys, functools, itertools, math, operator as op, re
+from collections import *
+words = lambda s: re.findall(r"[a-zA-z]+", s)
+Ints = lambda s: lmap(maybeint, re.findall(r"[+-]?\d+", s))
+stdin = lambda: sys.stdin.read().rstrip()
+lmap = lambda f, *x: list(map(f, *x))
+maybeint = lambda x: int(x) if re.match(r"^[+-]?\d+$", x) else x
+ints = lambda: lmap(maybeint, strs())
+grid = lambda: lmap(lambda x: lmap(maybeint, x), strs())
+strs = lambda: stdin().splitlines()
+splits = lambda s=None: lmap(lambda x: lmap(maybeint, x.split(s)), strs())
+chunks = lambda: lmap(lambda c: lmap(maybeint, c.splitlines()), stdin().split('\n\n'))
+slide = lambda l, n, s=1: [l[i:i + n] for i in range(0, len(l) - n, s)]
+
+ans = 0
+for s in strs():
+    a, b = s.split("|")
+    a = Ints(a)[1:]
+    b = Ints(b)
+    c = sum(x in a for x in b)
+    if c:
+        ans += 2 ** (c-1)
+print(ans)
+
