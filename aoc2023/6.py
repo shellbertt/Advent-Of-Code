24037855535d7fd818e52a08e2d863390a607a85
Kaaden RumanCam <kaadenruman@hotmail.com>
2023-12-06 00:26:14 -0500

    day 6, good stuff, top 200 both parts and good delta, my vim shortcuts to run the input sometimes only detect the sample version when i want both and that wasted a few seconds

---
+++ aoc2023/6.py
@@ -0,0 +1,28 @@
+import sys, functools, itertools, math, operator as op, re
+from collections import *
+words = lambda s: re.findall(r"[a-zA-z]+", s)
+Ints = lambda s: lmap(maybeint, re.findall(r"[+-]?\d+", s))
+stdin = lambda: sys.stdin.read().rstrip()
+lmap = lambda f, *x: list(map(f, *x))
+maybeint = lambda x: int(x) if re.match(r"^[+-]?\d+$", x) else x
+ints = lambda: lmap(maybeint, strs())
+grid = lambda: lmap(lambda x: lmap(maybeint, x), strs())
+strs = lambda: stdin().splitlines()
+splits = lambda s=None: lmap(lambda x: lmap(maybeint, x.split(s)), strs())
+chunks = lambda: lmap(lambda c: lmap(maybeint, c.splitlines()), stdin().split('\n\n'))
+slide = lambda l, n, s=1: [l[i:i + n] for i in range(0, len(l) - n, s)]
+
+time, dist = map(Ints, strs())
+n = len(time)
+ans = 1
+for i in range(n):
+    ways = 0
+    for t in range(1, time[i]):
+        s = t
+        d = s * (time[i]-t)
+        if d > dist[i]:
+            ways += 1
+    ans *= ways
+print(ans)
+    
+
