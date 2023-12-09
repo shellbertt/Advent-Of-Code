1ceb3c0f8f8d03044c64e79609564175566216ba
Kaaden RumanCam <kaadenruman@hotmail.com>
2023-12-09 00:18:54 -0500

    day 9, easy one for a friday shouldve coded it faster smh

---
+++ aoc2023/9.py
@@ -0,0 +1,27 @@
+import sys, functools, math, re
+from itertools import *
+from collections import *
+words = lambda s: re.findall(r"[a-zA-z]+", s)
+Ints = lambda s: lmap(maybeint, re.findall(r"[+-]?\d+", s))
+stdin = lambda: sys.stdin.read().rstrip()
+lmap = lambda f, *x: list(map(f, *x))
+maybeint = lambda x: int(x) if re.match(r"^[+-]?\d+$", x) else x
+ints = lambda: lmap(maybeint, strs())
+grid = lambda: lmap(lambda x: lmap(maybeint, x), strs())
+strs = lambda: stdin().splitlines()
+splits = lambda s=None: lmap(lambda x: lmap(maybeint, x.split(s)), strs())
+chunks = lambda: lmap(lambda c: lmap(maybeint, c.splitlines()), stdin().split('\n\n'))
+slide = lambda l, n, s=1: [l[i:i + n] for i in range(0, len(l) - n + 1, s)]
+
+ans = 0
+for s in strs():
+    a = Ints(s)
+    b = [y - x for x, y in zip(a, a[1:])]
+    m = [a, b]
+    while not all(x == 0 for x in b):
+        a = b
+        b = [y - x for x, y in zip(b, b[1:])]
+        m.append(b)
+    for x in m:
+        ans += x[-1]
+print(ans)
